d16d0e7515b5afd736700cf9b4b91f3679c9b312
savannah7704 <savannah7704@comcast.net>
2025-02-27 23:08:13 -0600

    Added files.

---
+++ pig.js
@@ -0,0 +1,139 @@
+let scores = { // object that initializes scores
+    playerRoll: 0,
+    playerTotalScore: 0,
+    playerCurrentScore: 0,
+    compRoll: 0,
+    compTotalScore: 0,
+    compCurrentScore: 0,
+}
+
+async function onLoadRoll() {
+    scores.playerCurrentScore = await rollDie(true)
+    scores.playerRoll = scores.playerCurrentScore
+    updateScores()
+}
+
+async function playerTurn() {
+    const roll = await rollDie()
+    if (roll == 1) {
+        scores.playerCurrentScore = 0
+        updateScores()
+        disablePlayerButtons()
+        computerTurn()
+    } else {
+        scores.playerCurrentScore += roll
+        updateScores()
+    }
+}
+
+async function computerTurn() {
+    let choice = Math.floor(Math.random() * 5)
+    while (choice != 0) {
+        const roll = await rollDie(false, false)
+        if (roll == 1) {
+            scores.compCurrentScore = 0
+            updateScores()
+            enablePlayerButtons()
+            return
+        } else {
+            scores.compCurrentScore += roll
+            updateScores()
+        }
+        choice = Math.floor(Math.random() * 5)
+    }
+
+    scores.compTotalScore += scores.compCurrentScore
+    scores.compCurrentScore = 0
+    updateScores()
+    checkWinner()
+    enablePlayerButtons()
+}
+
+async function rollDie(initialRoll = false, isPlayer = true) {
+    try {
+        const response = await fetch('https://dice-roller-node-js-gadwcxeyhua8ekhb.centralus-01.azurewebsites.net/')
+        const data = await response.json()
+        let roll = data.roll
+        if (initialRoll) {
+            return roll
+        } else {
+            if (roll == 1) {
+                if (isPlayer) {
+                    scores.compCurrentScore = 0
+                    scores.playerRoll = roll
+                    updateScores()
+                } else {
+                    scores.compCurrentScore = 0
+                    scores.compRoll = roll
+                    updateScores()
+                }
+            } else {
+                if (isPlayer) {
+                    scores.playerCurrentScore += roll
+                    scores.playerRoll = roll
+                    updateScores()
+                } else {
+                    scores.compCurrentScore += roll
+                    scores.compRoll = roll
+                    updateScores()
+                }
+            }
+        }
+        return roll 
+    } catch (error) {
+        console.error('Error: ', error)
+        return 1 //fallback in case of error
+    }
+}
+
+async function hold() {
+    scores.playerTotalScore += scores.playerCurrentScore
+    scores.playerCurrentScore = 0
+    updateScores()
+    checkWinner()
+    disablePlayerButtons()
+    computerTurn()
+}
+
+function checkWinner() {
+    if(scores.playerTotalScore >= 100){
+        alert("YOU WIN!!!!!");
+        reset();
+    } else if (scores.compTotalScore >= 100){
+        alert("Sorry, YOU LOSE!!!!!");
+        reset();
+    }
+
+}
+
+function disablePlayerButtons() {
+    document.getElementById("roll").disabled = true;
+    document.getElementById("hold").disabled = true;
+}
+
+function enablePlayerButtons() {
+    document.getElementById("roll").disabled = false;
+    document.getElementById("hold").disabled = false;
+}
+
+function updateScores() {
+    document.getElementById("your-roll").innerText = scores.playerRoll;
+    document.getElementById("your-current-score").innerText = scores.playerCurrentScore;
+    document.getElementById("your-total-score").innerText = scores.playerTotalScore;
+    document.getElementById("computer-roll").innerText = scores.compRoll;
+    document.getElementById("computer-current-score").innerText = scores.compCurrentScore;
+    document.getElementById("computer-total-score").innerText = scores.compTotalScore;
+}
+
+function reset() { //resets the game
+    scores = {
+        playerRoll: 0,
+        playerTotalScore: 0,
+        playerCurrentScore: 0,
+        compRoll: 0,
+        compTotalScore: 0,
+        compCurrentScore: 0,
+    }
+    updateScores();
+    enablePlayerButtons();
+}
